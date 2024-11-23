1aca68a148b720c7f89d9fdfaee3264a841410f3
俊龙 何 <1443002904@qq.com>
2024-11-23 16:34:42 +0800

    密码框绑定

--- PersonalNotepad/ViewModel/LoginViewModel.cs
+++ PersonalNotepad/ViewModel/LoginViewModel.cs
@@ -1,14 +1,21 @@
 ﻿using PersonalNotepad.Common;
+using PersonalNotepad.Model;
 using System.Windows;
 
 namespace PersonalNotepad.ViewModel
 {
     public class LoginViewModel
     {
+        public LoginModel LoginModel { get; set; } 
+
         public CommandBase CloseWindowCommand { get; set; }
 
         public LoginViewModel()
         {
+            this.LoginModel = new LoginModel();
+            this.LoginModel.UserName = "aaa";
+            this.LoginModel.Password = "123";
+
             this.CloseWindowCommand = new CommandBase();
             this.CloseWindowCommand.DoExecute = new Action<object>((o) =>
             {
